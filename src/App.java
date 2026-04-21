6bdfd2a056d9f980d4fc5e23ea5ff98a09fb8588
jenni-dz <dev.jennii0@gmail.com>
2026-04-21 13:46:10 -0400

    feat: estrutura lógica inicial da conta bancária, com opções de saldo, saque, depósito e encerrar, a serem selecionadas pelo usuário.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println(" / / / / Conta Corrente no Banco Brasil BR / / / /");
+        Scanner scanner = new Scanner(System.in);
+       
+        System.out.println("Escolha uma opção [1] Saldo  [2] Saque  [3] Depósito  [4] Encerrar");
+        
+        Integer opcao = scanner.nextInt();
+
+        switch (opcao) {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;    
+            case 3:
+                
+                break;
+            case 4:
+                System.out.println("Sessão encerrada.");
+                break;    
+            default:
+                System.out.println("Opção inválida, tente novamente!");
+                break;
+        }
+        scanner.close();
     }
 }
