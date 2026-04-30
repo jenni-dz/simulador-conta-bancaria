216de292d865f2446db8ef354852c29b6d3bd245
jenni-dz <dev.jennii0@gmail.com>
2026-04-30 15:07:25 -0400

    feat: modificação no menu principal, e redirecionamento correto às ações do menu principal.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
-import br.com.acoesconta.*;
+
+import br.com.acoesconta.Deposito;
+import br.com.acoesconta.Saldo;
+import br.com.acoesconta.Saque;
 
 public class App {
     public static void main(String[] args) {
@@ -10,17 +13,19 @@ public class App {
         Saque saque = new Saque();
 
         int opcao = -1;
+
         while (opcao != 0) {
             System.out.println(" / / / / Conta Corrente no Banco Brasil BR / / / /");
-            System.out.println("Escolha uma opção\n[ 1 ] Saldo\n[ 2 ] Saque\n[ 3 ] Depósito\n[ 4 ] Encerrar");
-
+            System.out.println("[ 1 ] Saldo\n[ 2 ] Saque\n[ 3 ] Depósito\n[ 0 ] Encerrar");
+            System.out.println("Escolha uma opção: ");
             opcao = scanner.nextInt();
 
             switch (opcao) {
-            //case 1 ->
-            //case 2 -> 
-            case 4 -> System.out.println("Sessão encerrada.");    
-            default -> System.out.println("Opção inválida, tente novamente!");
+                case 1 -> conta.exibirSaldo();
+                case 2 -> saque.realizarSaque(conta);
+                case 3 -> depos.realizarDeposito(conta);
+                case 0 -> System.out.println("Sessão encerrada.");  
+                default -> System.out.println("Opção inválida, tente novamente!");
             }
         }
         scanner.close();
